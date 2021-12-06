4b0731f3c06f4712497eafb8d57e2448c252dd1e
Varun Gupta <thevarungupt1@gmail.com>
2021-12-06 19:18:25 +0530

    Catalog service implemented

---
+++ src/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(IProductRepository repository, ILogger<CatalogController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+    }
+}
---
+++ src/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,59 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct =  productCollection.Find(p => true).Any();
+            if(!existProduct)
+            {
+                productCollection.InsertManyAsync(GetSampleProducts());
+            }
+        }
+
+        public static IEnumerable<Product> GetSampleProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Id ="5de491c069ee2123b48daae8",
+                    Name = "Laptop 1",
+                    Summary = "Laptop summary 1",
+                    Description = "Laptop Description 1",
+                    ImageFile = "no-image.jpg",
+                    Price = 900.00M,
+                    Category = "Laptop"
+                },
+                new Product()
+                {
+                    Id ="5de491c069ee2123b48daae1",
+                    Name = "Mobile 1",
+                    Summary = "Mobile summary 1",
+                    Description = "Mobile Description 1",
+                    ImageFile = "no-image.jpg",
+                    Price = 800.00M,
+                    Category = "Mobile"
+                },
+                new Product()
+                {
+                    Id ="5de491c069ee2123b48daae2",
+                    Name = "Desktop 1",
+                    Summary = "Desktop summary 1",
+                    Description = "Desktop Description 1",
+                    ImageFile = "no-image.jpg",
+                    Price = 600.00M,
+                    Category = "Desktop"
+                },
+            };
+        }
+   
+    }
+}
